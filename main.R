4d8ed6501a94702b18879614d819255cec1e1c38
Trinley Palmo <t.palmo@hotmail.com>
2025-03-27 00:36:59 -0400

    Added RNNs with single and two layers

--- main.R
+++ main.R
@@ -82,6 +82,24 @@ lengths <- nchar(training_data)
 #Distribution of lengths
 summary(lengths)
 
+# View the distribution of the lengths
+# make lengths a data frame
+tweet_df <- data.frame(length = lengths)
+
+# Compute quantiles for 5% and 95%
+lower_bound <- quantile(tweet_df$length, 0.05)
+upper_bound <- quantile(tweet_df$length, 0.95)
+
+tweet_df$category <- ifelse(tweet_df$length < lower_bound | tweet_df$length > upper_bound, "Outside", "Middle 90%")
+
+# Make a histogram with ggplot to show distribution and inside 90%
+p <- ggplot(tweet_df, aes(x = length, fill = category)) +
+  geom_histogram(binwidth = 15, color = "black", alpha = 0.7) +
+  scale_fill_manual(values = c("Middle 90%" = "blue", "Outside" = "red")) +
+  labs(title = "Distribution of Tweet Length", x = "Tweet Length", y = "Count") +
+  theme_minimal()
+print(p)
+
 #pick the 90th percentile to pad to
 # this covers most sequences, but will exclude any outliers
 max_length<- quantile(lengths, 0.9)
@@ -119,6 +137,8 @@ model <- keras_model_sequential() %>%
 
 model
 
+#### C2.4 ####
+
 #Compiling the model
 model %>% compile(
   optimizer = "rmsprop",
@@ -135,3 +155,83 @@ history <- model %>% fit(
   validation_split = 0.2
 )
 
+#### C2.5 ####
+model_rnn <- keras_model_sequential() %>%
+  # embedding layer
+  layer_embedding(input_dim = max_features, output_dim = 8, input_length = max_length) %>%
+  # recurrent layer
+  layer_simple_rnn(units = 128) %>%
+  # One hidden layer
+  layer_dense(units = 512, activation = "relu") %>% 
+  # output layer
+  layer_dense(units = 5, activation = "softmax")
+
+model_rnn
+
+#Compiling the model
+model_rnn %>% compile(
+  optimizer = "rmsprop",
+  loss = "categorical_crossentropy",
+  metrics = c("acc")
+)
+
+#training the model
+history_rnn <- model_rnn %>% fit(
+  training_data,
+  training_labels,
+  epochs = 10,
+  batch_size = 32,
+  validation_split = 0.2
+)
+
+# set up testing labels -> ADD EARLIER TO MAIN !!!
+testing_labels <- (as.numeric(testing_labels)-1) %>% 
+  to_categorical()
+
+# test the model 
+perf_rnn <- evaluate(model_rnn, testing_data, testing_labels)
+perf_rnn
+
+save(history_rnn, perf_rnn, file = "rnn_res.RData")
+plot(history_rnn) 
+## Should try higher epochs
+
+#### C2.6 ####
+model_rnn2 <- keras_model_sequential() %>%
+  # embedding layer
+  layer_embedding(input_dim = max_features, output_dim = 8, input_length = max_length) %>%
+  # 1st recurrent layer
+  layer_simple_rnn(units = 128, return_sequences = T) %>%
+  # 2nd recurrent layer
+  layer_simple_rnn(units = 64) %>%
+  # One hidden layer
+  layer_dense(units = 512, activation = "relu") %>% 
+  # output layer
+  layer_dense(units = 5, activation = "softmax")
+
+model_rnn2
+
+#Compiling the model
+model_rnn2 %>% compile(
+  optimizer = "rmsprop",
+  loss = "categorical_crossentropy",
+  metrics = c("acc")
+)
+
+#training the model
+history <- model_rnn2 %>% fit(
+  training_data,
+  training_labels,
+  epochs = 20,
+  batch_size = 32,
+  validation_split = 0.2
+)
+
+
+# test the model 
+perf_rnn2 <- evaluate(model_rnn2, testing_data, testing_labels)
+perf_rnn2
+
+save(history, perf_rnn2, file = "rnn2_res.RData")
+plot(history)
+## I think epoch = 12 is enough or max 15
